7d7848ce4e3047036528d27d5bd20e043be1e3f0
river0203 <65986354+river0203@users.noreply.github.com>
2024-07-19 15:36:23 +0900

    follow mob
    trigger event가 발생했을 때 추적을 시작함 수정요함

---
+++ Natr_Summer/Assets/Scripts/Mob/FishMob.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMob : Mob
+{
+    private Rigidbody2D rb;
+    private Transform targe;
+
+    private float moveSpeed = 3f;
+    private float contactDistance = 2f;
+
+    private bool follow = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        targe = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowTarget();
+    }
+    public void FollowTarget()
+    {
+        if(Vector2.Distance(transform.position, targe.position) > contactDistance && follow)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targe.position, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+    public override void attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void hit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void move()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        follow = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        follow = false;
+    }
+}
